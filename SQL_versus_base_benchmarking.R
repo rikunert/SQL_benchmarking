72e2f1228f0367b74eeea84df0a461f0f8dd5f83
Richard Kunert <rikunert@gmail.com>
2017-10-30 11:53:13 +0100

    Tufte boxplots

--- SQL_versus_base_benchmarking.R
+++ SQL_versus_base_benchmarking.R
@@ -5,14 +5,11 @@
 ##########################################################################
 # LOAD LIBRARIES
 
-
-if(!require(plyr)){install.packages('plyr')} #plotting
-library(plyr)
+if(!require(ggthemes)){install.packages('ggthemes')} #plotting
+library(ggthemes)
 
 if(!require(ggplot2)){install.packages('ggplot2')} #plotting
 library(ggplot2)
-
-source('https://raw.githubusercontent.com/FredHasselman/toolboxR/master/C-3PR.R')#vioQtile function
 
 if(!require(microbenchmark)){install.packages('microbenchmark')} #
 library(microbenchmark)
@@ -22,7 +19,6 @@ library(RSQLite)
 
 if(!require(DBI)){install.packages('DBI')} #interface with SQL
 library(DBI)
-
 
 ##########################################################################
 # DATA BASE SET UP
@@ -34,25 +30,24 @@ db_mem <- dbConnect(RSQLite::SQLite(), "file::memory:")
 dbWriteTable(db_disk, "iris", iris)
 dbWriteTable(db_mem, "iris", iris)
 
-
 ##########################################################################
 # CUSTOM FUNCTIONS
 fun_disk <- function(query, db_disk. = db_disk) dbGetQuery(db_disk., query)
 fun_mem <- function(query, db_mem. = db_mem) dbGetQuery(db_mem., query)
-fun_plot <- function(dat_box=tm, y_lim = c(0.1, 1000)){
+fun_plot <- function(dat_box=tm, y_lim = c(10, 50000)){
 
   dat_box$time = dat_box$time/1000
   
   A = ggplot(dat_box, aes(x = expr, group = expr, y = time)) +
-    geom_boxplot(notch = F) +
+    geom_tufteboxplot(median.type = "line", whisker.type = 'line', hoffset = 0, width = 3) +
     xlab("") + 
     ylab("Execution Time (microseconds)") + 
-    scale_y_log10(limits = y_lim) +
+    scale_y_log10(limits = y_lim, breaks = 10^(1:4)) +
     scale_x_discrete(labels = c("SQL\non disk",
                                 "SQL\nin memory",
                                 "base R\nin memory\n(variation 1)", 
                                 "base R\nin memory\n(variation 2)")) +
-    gg.theme("clean") + 
+    theme_tufte(ticks = F) + 
     theme(axis.text.x = element_text(size=12))
   A
   
@@ -62,9 +57,12 @@ fun_plot <- function(dat_box=tm, y_lim = c(0.1, 1000)){
 query = 'SELECT * FROM iris LIMIT 5'
 fun_base1 <- function(data = iris) head(data, 5)
 fun_base2 <- function(data = iris) data[1:5,]
+
 tm = microbenchmark(fun_disk(query), fun_mem(query), fun_base1(), fun_base2())
-boxplot(tm)
-fun_plot(dat_box = tm, y_lim = c(10, 1000)) + ggtitle('Basic query')
+
+fun_plot(dat_box = tm) + ggtitle('Basic query')
+ggsave('SQL_R_bench_1.png',width = 12.9, height = 5.42, scale = 0.7, dpi = 1000)
+
 dbGetQuery(db_mem, query)
 
 ##########################################################################
@@ -72,9 +70,12 @@ dbGetQuery(db_mem, query)
 query = 'SELECT species, AVG("Petal.Length") FROM iris GROUP BY species'
 fun_base1 <- function(data = iris) aggregate(data$Petal.Length~data$Species, FUN=mean)
 fun_base2 <- function(data = iris) by(data$Petal.Length, data$Species, mean)
+
 tm = microbenchmark(fun_disk(query), fun_mem(query), fun_base1(), fun_base2())
-boxplot(tm)
-fun_plot(dat_box = tm, y_lim = c(100, 10000)) + ggtitle('Basic aggregation')
+
+fun_plot(dat_box = tm) + ggtitle('Basic aggregation')
+ggsave('SQL_R_bench_2.png',width = 12.9, height = 5.42, scale = 0.7, dpi = 1000)
+
 dbGetQuery(db_mem, query)
 
 ##########################################################################
@@ -82,9 +83,12 @@ dbGetQuery(db_mem, query)
 query = 'SELECT species, COUNT(*) FROM iris WHERE "Petal.Length" >= 2'
 fun_base1 <- function(data = iris) sum(data$Petal.Length > 2)
 fun_base2 <- function(data = iris) length(data$Petal.Length[data$Petal.Length > 2])
+
 tm = microbenchmark(fun_disk(query), fun_mem(query), fun_base1(), fun_base2())
-boxplot(tm)
-fun_plot(dat_box = tm, y_lim = c(10, 1000)) + ggtitle('Basic aggregation on conditional')
+
+fun_plot(dat_box = tm) + ggtitle('Basic aggregation on conditional')
+ggsave('SQL_R_bench_3.png',width = 12.9, height = 5.42, scale = 0.7, dpi = 1000)
+
 dbGetQuery(db_mem, query)
 
 ##########################################################################
@@ -98,6 +102,7 @@ END AS size,
 COUNT(*)
 FROM iris
 GROUP BY 1, 2;'
+
 fun_base1 <- function(data = iris) {
   size = rep('intermediate', nrow(data))
   size[data$Petal.Length < 2] = 'small'
@@ -105,7 +110,8 @@ fun_base1 <- function(data = iris) {
   aggregate(rep(1, nrow(data)),#just a vector of 1s which will get added up
             by=list(Species = data$Species, size = size),
             FUN=sum)
-  }
+}
+
 fun_base2 <- function(data = iris) {
   out = data.frame(Species = rep(unique(data$Species), each = 3),
                    size = rep(c('big', 'intermediate', 'small'), 3),
@@ -116,9 +122,12 @@ fun_base2 <- function(data = iris) {
     out$count[out$Species == Species & out$size == 'big'] = sum(data$Species == Species & data$Petal.Length > 5)
   }
 }
+
 tm = microbenchmark(fun_disk(query), fun_mem(query), fun_base1(), fun_base2())
-boxplot(tm)
-fun_plot(dat_box = tm, y_lim = c(100, 10000)) + ggtitle('Aggregation of temporary variable on conditional')
+
+fun_plot(dat_box = tm) + ggtitle('Aggregation of temporary variable on conditional')
+ggsave('SQL_R_bench_4.png',width = 12.9, height = 5.42, scale = 0.7, dpi = 1000)
+
 dbGetQuery(db_mem, query)
 
 ##########################################################################
@@ -131,16 +140,16 @@ VALUES
 (\'versicolor\', \'May\', \'July\'),
 (\'virginica\', \'April\', \'May\'),
 (\'pallida\', \'May\', \'June\');'
+
 fun_disk_exe3 <- function(statements, db_disk. = db_disk) lapply(1:3, function(x) dbExecute(db_disk., statements[x]))
-# {dbExecute(db_disk., statements[1]); 
-#   dbExecute(db_disk., statements[2])
-#   dbExecute(db_disk., statements[3])}
 fun_mem_exe3 <- function(statements, db_mem. = db_mem) lapply(1:3, function(x) dbExecute(db_mem., statements[x]))
+
 fun_base1 <- function(data = iris) {
   flowers = data.frame(species = c('setosa', 'versicolor', 'virginica', 'pallida'),
                        bloom_start = c('June', 'May', 'April', 'May'),
                        bloom_end = c('July', 'July', 'May', 'June'))
 }
+
 fun_base2 <- function(data = iris) {
   flowers = rbind(c('setosa','June','July'),
                   c('versicolor', 'May', 'July'),
@@ -150,18 +159,22 @@ fun_base2 <- function(data = iris) {
   colnames(flowers) = c('species', 'bloom_start', 'bloom_end')
   as.data.frame(flowers)
 }
+
 tm = microbenchmark(fun_disk_exe3(c(query1, query2, query3)), fun_mem_exe3(c(query1, query2, query3)), fun_base1(), fun_base2())
-boxplot(tm)
-fun_plot(dat_box = tm, y_lim = c(100, 10000)) + ggtitle('Create table and add to data base')
+
+fun_plot(dat_box = tm) + ggtitle('Create table and add to data base')
+ggsave('SQL_R_bench_5.png',width = 12.9, height = 5.42, scale = 0.7, dpi = 1000)
 
 ##########################################################################
 #6: joining 2 tables
 query = 'SELECT * FROM flowers
 JOIN iris ON
 flowers.species = iris.species'
+
 flowers = data.frame(species = c('setosa', 'versicolor', 'virginica', 'pallida'),
                      bloom_start = c('June', 'May', 'April', 'May'),
                      bloom_end = c('July', 'July', 'May', 'June'))
+
 fun_base1 <- function(data1 = iris, data2 = flowers) {
   data1$bloom_start = NA
   data1$bloom_end = NA
@@ -171,12 +184,16 @@ fun_base1 <- function(data1 = iris, data2 = flowers) {
   }
   return(data1)
 }
+
 fun_base2 <- function(data1 = iris, data2 = flowers) {
   merge(data1, data2, by.x = 'Species', by.y = 'species')
 }
+
 tm = microbenchmark(fun_disk(query), fun_mem(query), fun_base1(), fun_base2())
-boxplot(tm)
-fun_plot(dat_box = tm, y_lim = c(100, 45000)) + ggtitle('Joining two tables')
+
+fun_plot(dat_box = tm) + ggtitle('Joining two tables')
+ggsave('SQL_R_bench_6.png',width = 12.9, height = 5.42, scale = 0.7, dpi = 1000)
+
 dbGetQuery(db_disk, query)
 
 ##########################################################################
@@ -185,6 +202,7 @@ query = 'SELECT flowers.bloom_end, AVG(iris."petal.length") FROM flowers
 JOIN iris ON
 flowers.species = iris.species
 GROUP BY flowers.bloom_end'
+
 fun_base1 <- function(data1 = iris, data2 = flowers) {
   out = matrix(rep(NA, length(unique(data2$bloom_end)) * 2), nrow = length(unique(data2$bloom_end)), ncol = 2)
   for (i in 1:length(unique(data2$bloom_end))){
@@ -194,11 +212,15 @@ fun_base1 <- function(data1 = iris, data2 = flowers) {
                     data1$Species)]))
   }
 }
+
 fun_base2 <- function(data1 = iris, data2 = flowers) {
   x = merge(data1, data2, by.x = 'Species', by.y = 'species')
   aggregate(x = x, by=list(x$bloom_end), FUN=mean)[,c(1, 5)]
 }
+
 tm = microbenchmark(fun_disk(query), fun_mem(query), fun_base1(), fun_base2())
-boxplot(tm)
-fun_plot(dat_box = tm, y_lim = c(100, 10000)) + ggtitle('Joining two tables and aggregation')
+
+fun_plot(dat_box = tm) + ggtitle('Joining two tables and aggregation')
+ggsave('SQL_R_bench_7.png',width = 12.9, height = 5.42, scale = 0.7, dpi = 1000)
+
 dbGetQuery(db_disk, query)
